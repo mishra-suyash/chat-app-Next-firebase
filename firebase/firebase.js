d5406576400a468762c0cb31dace0191a9110cd7
Suyash Mishra <mishrasuyash@hotmail.com>
2025-08-04 14:37:57 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,17 +4,22 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration, now read from environment variables
+// For more information on how to access environment variables visit:
+// https://nextjs.org/docs/basic-features/environment-variables
 const firebaseConfig = {
-  apiKey: "AIzaSyB-tm3BUEWKOXcOV_hkI6YKOFpwOQQKYaE",
-  authDomain: "chat-app-next-firebase.firebaseapp.com",
-  projectId: "chat-app-next-firebase",
-  storageBucket: "chat-app-next-firebase.appspot.com",
-  messagingSenderId: "815351332242",
-  appId: "1:815351332242:web:a5af08eefba871c1a3554b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export the Firebase services that you need in your application
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
